bcc183efa80ff2e7fd2383198f4bded42af4e913
ghodawalaaman <ghodawalaaman@gmail.com>
2023-03-11 11:29:47 +0530

    Some changes

---
+++ python-group-project/music.py
@@ -0,0 +1,15 @@
+import playsound
+# playsound is library to play a sound independent of platform
+
+class Music:
+    # Contains metadata of Music. ie, when it's added, singer name etc.
+    def __init__(self):
+        return None
+class User:
+    # It has user's data like username, password, favourite musics.
+    def __init__(self):
+        return None
+class App:
+    # Main application class
+    def __init__(self):
+        return None
